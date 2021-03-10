02f640b710a1ff4d3572804c6302fea2d4888f6a
akgupta-47 <akshatgupta3214@gmail.com>
2021-03-11 00:55:58 +0530

    try catch added

--- bot.js
+++ bot.js
@@ -23,25 +23,32 @@ client.on('message', (msg) => {
 */
 const prefix = '#';
 let link, subject, objTime;
-
+// add try catch for thur
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    try{
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
-    const schedule = args.map(element => element.toLowerCase());
-    // console.log(schedule);
-    objTime = time_table[schedule[0]][schedule[1]];
+        const spaceRemovedmessageContent = message.content.replace(/\s+/g, ' ');
+        console.log(spaceRemovedmessageContent);
+        const args = spaceRemovedmessageContent.slice(prefix.length).trim().split(' ');
+        console.log(args);
+        const command = args.shift().toLowerCase();
+        const schedule = args.map(element => element.toLowerCase());
+        // console.log(schedule);
+        objTime = time_table[schedule[0]][schedule[1]];
 
-    if (command === 'zoom') {
-        if (schedule.length < 2) {
-            return message.channel.send(`You didn't provide enough arguments, ${message.author}!`);
+        if (command === 'zoom') {
+            if (schedule.length < 2) {
+                return message.channel.send(`You didn't provide enough arguments, ${message.author}!`);
+            }
+            // console.log(objTime[2]['link']);
+            for(i =0; i< objTime.length; i++) {
+                link = objTime[i]['link'];
+                subject = objTime[i]['time-sub'];
+                message.channel.send(`${subject} : https://tiet.zoom.us/my/${link} `);
+            }
         }
-        // console.log(objTime[2]['link']);
-        for(i =0; i< objTime.length; i++) {
-            link = objTime[i]['link'];
-            subject = objTime[i]['time-sub'];
-            message.channel.send(`${subject} : https://tiet.zoom.us/my/${link} `);
-        }
+    }catch(err) {
+        message.reply('check your command either the day or batch are wrong');
     }
 });
